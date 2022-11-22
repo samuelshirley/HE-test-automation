40ae4fd1f3701bd6dabab508ea177673dd4a0497
Samuel Shirley <37374497+samuelshirley@users.noreply.github.com>
2022-11-22 13:22:18 -0600

    updated it block to match ticket AC

--- cypress/e2e/Github/Search/unauthenticated-user.spec.cy.js
+++ cypress/e2e/Github/Search/unauthenticated-user.spec.cy.js
@@ -6,14 +6,14 @@ describe('As a guest (not logged-in), when I search github for the term "create-
       cy.runSearch();
     });
 
-    it('should search create-react-app and find a count of results', () => {
+    it('a count of results', () => {
       cy.get(search['codesearch-results']).should(
         'contain',
         'repository results'
       );
     });
 
-    it('should find facebook/create-react-app at top of search results', () => {
+    it('facebook/create-react-app at top of search results', () => {
       cy.url().should('contain', '/search?q=create-react-app');
       cy.get(search['repo-list']).each((repoItem) => {
         cy.get(repoItem[0].children[0])
@@ -22,7 +22,7 @@ describe('As a guest (not logged-in), when I search github for the term "create-
       });
     });
 
-    it('should find create-react-app in header of each search result on first page', () => {
+    it('create-react-app in header of each search result on first page', () => {
       cy.get(search['repo-list']).each(() => {
         cy.get(search['repo-list-item'])
           .find(search['repo-item-header'])
@@ -37,7 +37,7 @@ describe('As a guest (not logged-in), when I search github for the term "create-
       cy.viewport('iphone-x');
     });
 
-    it('should check for mobile hamburger and search bar', () => {
+    it('a count of results', () => {
       cy.get(search['codesearch-results']).should(
         'contain',
         'repository results'
