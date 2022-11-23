b4b3fb7f6feef9014721015ee073dd92114604c8
Samuel Shirley <37374497+samuelshirley@users.noreply.github.com>
2022-11-22 21:29:19 -0600

    token expired

--- API-Tests/test.spec.js
+++ API-Tests/test.spec.js
@@ -1,7 +1,7 @@
 const { Octokit } = require('octokit');
 
 const octokit = new Octokit({
-  auth: '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',
+  auth: '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',
 });
 
 test('Access a public repo with ocokit / github API', async () => {
@@ -9,6 +9,7 @@ test('Access a public repo with ocokit / github API', async () => {
     owner: 'SamuelShirley',
     repo: 'HE-test-automation',
   });
+  console.log(res);
   expect(res.status).toBe(200);
   expect(res.url).toBe(
     'https://api.github.com/repos/SamuelShirley/HE-test-automation'
